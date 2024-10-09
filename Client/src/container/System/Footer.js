e1f3436109788362a78c1f92f3958f3a2dabec21
Nguyễn Đức Huy - PH33852 <139211478+duchuy112233@users.noreply.github.com>
2024-10-09 17:55:28 +0700

    Header-footer

---
+++ Client/src/container/System/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="py-4 bg-light mt-auto">
+            <div className="container-fluid px-4">
+                <div className="d-flex align-items-center justify-content-between small">
+                    <div className="text-muted">Bản quyền © FREST FITS</div>
+                    
+                </div>
+            </div>
+        </footer>
+    )
+}
+export default Footer;
